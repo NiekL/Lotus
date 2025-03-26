658aa3d8ea42e902964748d07f7de2b7f552c300
Niek <niekluttikhof8@gmail.com>
2025-03-26 20:52:22 +0100

    Show password
    Show password

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -15,6 +15,8 @@ const form = useForm({
     password_confirmation: '',
 });
 
+const showPassword = ref({});
+
 const updatePassword = () => {
     form.put(route('password.update'), {
         preserveScroll: true,
@@ -44,46 +46,65 @@ const updatePassword = () => {
         </header>
 
         <form @submit.prevent="updatePassword" class="mt-6 space-y-6">
-            <div>
+            <div class="relative">
                 <InputLabel for="current_password" value="Huidig wachtwoord" />
 
                 <TextInput
                     id="current_password"
                     ref="currentPasswordInput"
                     v-model="form.current_password"
-                    type="password"
+                    :type="showPassword['current_password'] ? 'text' : 'password'"
                     class="mt-1 block w-full"
                     autocomplete="current-password"
                 />
-
+                <button
+                    type="button"
+                    @click="showPassword['current_password'] = !showPassword['current_password']"
+                    class="absolute bottom-1 h-fit right-0 flex items-end pr-3 pb-2"
+                >
+                    <i :class="showPassword['current_password'] ? 'fa-eye-slash' : 'fa-eye'" class="text-gray-500 hover:text-gray-700 fa-regular" />
+                </button>
                 <InputError :message="form.errors.current_password" class="mt-2" />
             </div>
 
-            <div>
-                <InputLabel for="password" value="New Password" />
+            <div class="relative">
+                <InputLabel for="password" value="Nieuw wachtwoord" />
 
                 <TextInput
                     id="password"
                     ref="passwordInput"
                     v-model="form.password"
-                    type="password"
+                    :type="showPassword['password'] ? 'text' : 'password'"
                     class="mt-1 block w-full"
                     autocomplete="new-password"
                 />
-
+                <button
+                    type="button"
+                    @click="showPassword['password'] = !showPassword['password']"
+                    class="absolute bottom-1 h-fit right-0 flex items-end pr-3 pb-2"
+                >
+                    <i :class="showPassword['password'] ? 'fa-eye-slash' : 'fa-eye'" class="text-gray-500 hover:text-gray-700 fa-regular" />
+                </button>
                 <InputError :message="form.errors.password" class="mt-2" />
             </div>
 
-            <div>
+            <div class="relative">
                 <InputLabel for="password_confirmation" value="Bevestig wachtwoord" />
 
                 <TextInput
                     id="password_confirmation"
                     v-model="form.password_confirmation"
-                    type="password"
+                    :type="showPassword['password_confirmation'] ? 'text' : 'password'"
                     class="mt-1 block w-full"
                     autocomplete="new-password"
                 />
+                <button
+                    type="button"
+                    @click="showPassword['password_confirmation'] = !showPassword['password_confirmation']"
+                    class="absolute bottom-1 h-fit right-0 flex items-end pr-3 pb-2"
+                >
+                    <i :class="showPassword['password_confirmation'] ? 'fa-eye-slash' : 'fa-eye'" class="text-gray-500 hover:text-gray-700 fa-regular" />
+                </button>
 
                 <InputError :message="form.errors.password_confirmation" class="mt-2" />
             </div>
