c11d1b0ec81fe09569b3a67405170a6bfba066d7
Niek <niekluttikhof8@gmail.com>
2025-04-07 19:56:50 +0200

    Mail aangepast

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,5 +26,22 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            $url = url(route('password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+
+            return (new MailMessage)
+                ->subject('Wachtwoord opnieuw instellen Lotus Planner')
+                ->greeting('Hallo!')
+                ->line('Lotuskring de Blauwvinger heeft een nieuw systeem voor het doen van lotus aanvragen.')
+                ->line('Hiervoor is het nodig dat bestaande klanten een nieuw wachtwoord in moeten stellen')
+                ->line('Je hebt aangegeven je wachtwoord opnieuw te willen instellen of je bent nog een gebruiker uit het oude systeem.')
+                ->action('Stel wachtwoord opnieuw in', $url)
+                ->line('Deze link is geldig gedurende 1 week. Is de link verlopen? Geen probleem, via wachtwoord vergeten kunt u een nieuwe herstelmail aanvragen.')
+                ->line('Heb je dit niet aangevraagd? Dan hoef je niets te doen.');
+        });
     }
 }
