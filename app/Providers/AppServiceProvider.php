5faad6f1184406020cc911c8ca91bdaf1b1027ca
Niek <niekluttikhof8@gmail.com>
2025-04-07 20:01:13 +0200

    Mail aangepast

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,12 +36,15 @@ class AppServiceProvider extends ServiceProvider
             return (new MailMessage)
                 ->subject('Wachtwoord opnieuw instellen Lotus Planner')
                 ->greeting('Hallo!')
-                ->line('Lotuskring de Blauwvinger heeft een nieuw systeem voor het doen van lotus aanvragen.')
-                ->line('Hiervoor is het nodig dat bestaande klanten een nieuw wachtwoord in moeten stellen')
+                ->line('Lotuskring de Blauwvinger heeft een nieuw systeem voor het indienen van lotus aanvragen.')
+                ->line('Hiervoor is het nodig dat bestaande klanten een nieuw wachtwoord in moeten stellen.')
                 ->line('Je hebt aangegeven je wachtwoord opnieuw te willen instellen of je bent nog een gebruiker uit het oude systeem.')
                 ->action('Stel wachtwoord opnieuw in', $url)
                 ->line('Deze link is geldig gedurende 1 week. Is de link verlopen? Geen probleem, via wachtwoord vergeten kunt u een nieuwe herstelmail aanvragen.')
-                ->line('Heb je dit niet aangevraagd? Dan hoef je niets te doen.');
+//                ->line('Heb je dit niet aangevraagd? Dan hoef je niets te doen.')
+                ->salutation('Met vriendelijke groet, <br>Lotuskring de Blauwvinger')
+                ->line('Werkt de knop niet? Kopieer en plak de onderstaande URL in je browser:')
+                    ->line($url);
         });
     }
 }
